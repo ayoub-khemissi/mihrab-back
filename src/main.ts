5946c2f69931cf1e8416b756a3757dbd0c976b09
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-05-27 15:38:44 +0200

    feat: Main file server config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
+import { ValidationPipe } from '@nestjs/common';
+
+const { MIHRAB_BACK_PORT, MIHRAB_BACK_HOST } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(json());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(MIHRAB_BACK_PORT || 8080, MIHRAB_BACK_HOST || 'localhost');
 }
+
 bootstrap();
